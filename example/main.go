d75819dd55fea8af0005b53b6933a8e2c9c80e1a
ysqi <nerthus03@nerthus03deiMac.local>
2017-09-23 18:29:25 +0800

    add demo

---
+++ example/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"io/ioutil"
+	"fmt"
+
+	"github.com/golang/example/stringutil"
+)
+
+func main(){
+	var notGood = "not good"
+	_=notGood
+
+	var n= note() 
+	fmt.Println(n) 
+}
+
+func reverse(src string) string{
+	return stringutil.Reverse(src)
+}
+func note() string{
+	if false {
+		fmt.Println("nerver")
+	}
+	return "Today is nice day!"
+}
+
+func bad() {
+	fmt.Sprintf("is %s or %s","bad")
+}
+
+func bad2(){
+	var s1 string = "S1"
+	s1 = s1 // 自赋值
+}
+
+func bad3(){
+	f,_:=ioutil.TempFile("","bad")
+	defer f.Close()
+}
---
+++ example/main_test.go
@@ -0,0 +1,28 @@
+package main 
+
+import (
+	"testing"
+)
+
+func TestNote(t *testing.T){
+
+	n:=note()
+	if n=="Today is nice day!"{
+		t.Error(n)
+	}
+}
+
+func TestReverse(t *testing.T) {
+	for _, c := range []struct {
+		in, want string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{"Hello, 世界", "界世 ,olleH"},
+		{"", ""},
+	} {
+		got := reverse(c.in)
+		if got != c.want {
+			t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
